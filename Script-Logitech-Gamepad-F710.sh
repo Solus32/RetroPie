603d79f2c969958c9600199e9d5c05cf02b87228
José Luis <Solus32@users.noreply.github.com>
2019-05-09 12:27:58 -0500

    Add files via upload

--- Script-Logitech-Gamepad-F710.sh
+++ Script-Logitech-Gamepad-F710.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 wget -c https://raw.githubusercontent.com/Solus32/RetroPie/master/Logitech%20Gamepad%20F710.cfg -O /opt/retropie/configs/all/retroarch/autoconfig/"Logitech Gamepad F710.cfg"
+wget -c https://raw.githubusercontent.com/Solus32/RetroPie/master/es_input_Logitech_Gamepad_F710.cfg -O /opt/retropie/configs/all/emulationstation/es_input.cfg
